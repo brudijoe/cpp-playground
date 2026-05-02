8e9f2a59c645470452b85c096dfcd85844534aa9
brudijoe <doningo@ymail.com>
2026-05-02 10:41:06 +0200

    refactor: remove redundant code and simplify method

--- projects/003_guess_the_number/win_condition.cpp
+++ projects/003_guess_the_number/win_condition.cpp
@@ -4,14 +4,14 @@
 
 void winCondition(int lucky_number, int guessed_number, Statistic &statistic)
 {
+    statistic.setSumOfGames(statistic.getSumOfGames() + 1);
+
     if (lucky_number == guessed_number)
     {
         std::cout << "Congratulations, you've won!" << "\n\n";
         statistic.setGamesWon(statistic.getGamesWon() + 1);
-        statistic.setSumOfGames(statistic.getSumOfGames() + 1);
         return;
     }
     std::cout << "Sorry, you've lost!" << "\n\n";
     statistic.setGamesLost(statistic.getGamesLost() + 1);
-    statistic.setSumOfGames(statistic.getSumOfGames() + 1);
 }
